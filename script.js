3bc7ab67f0be8d4912a65592afc484796ca85ea0
ns5376 <116809834+ns5376@users.noreply.github.com>
2024-06-13 05:15:01 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    const goggles = document.getElementById('vr-goggles');
-    goggles.addEventListener('animationend', function() {
-        goggles.style.display = 'none';
-        document.getElementById('main-content').style.display = 'flex';
-    });
-});
